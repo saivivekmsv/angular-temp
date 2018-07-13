0e73427f718ec98d084923b7f6b18988c1fc156d
sai vivek <saivivek.msv@gmail.com>
2018-07-13 15:42:23 +0530

    add header and scss style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'angular';
+  clickHandler() {
+    alert('I am cliked');
+  }
 }
